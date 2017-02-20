e18b2389b1940c4807a6989b600a36496df269c6
Will Skelton <william.f.skelton@outlook.com>
2017-02-20 14:26:15 -0800

    server is now working

---
+++ lab-will/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./model/client.js');
+// const PORT = process.env.PORT || 3000;
+
+// Random Port;
+const PORT = Math.floor(Math.random() * 9999-1000);
+
+const server = net.createServer();
+const ee = new EE();
+
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+
+  socket.on('data', function(data) {
+    const command = data.toString();
+    console.log(command);
+  });
+});
+
+server.listen(PORT, function() {
+  console.log('server up:', PORT);
+})
