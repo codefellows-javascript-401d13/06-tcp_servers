c906e36def25e1201e5d682120fbe2995f2d4e2b
Dana Kulp <dkulp23@gmail.com>
2017-02-20 13:48:54 -0800

    Created event and emitter to instantiate Client object with connected socket.

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ const Client = require(`${__dirname}/model/client.js`);
 const EE = require('events');
 const ee = new EE();
 
+const chatGroup = [];
 
-server.on('connection', function(socket) {
+ee.on('login', function(socket) {
   const client = new Client(socket);
-  console.log(client);
+  chatGroup.push(client);
 });
 
+
+server.on('connection', function(socket) {
+  ee.emit('login', socket);
+});
 
 server.listen(PORT, function() {
   console.log(`Server started on port: ${PORT}`);
