22663fffea20bd7e124816e1c3ad0cae0306aaaf
Gary Lundgren <builtbygl@gmail.com>
2017-02-20 13:59:21 -0800

    saving initial build

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./model/client.js');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+const ee = new EE();
+
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+  pool.push(client);
+
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+
+    if (command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split().slice(1).join(' '));
+      return;
+    };
+
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, function() {
+  console.log('server up and running on:', PORT);
+});
