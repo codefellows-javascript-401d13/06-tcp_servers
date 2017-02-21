6449f8f7b099026b2988b867cbf8f727545fcb87
Gary Lundgren <builtbygl@gmail.com>
2017-02-20 17:38:00 -0800

    not printing message

--- server.js
+++ server.js
@@ -7,6 +7,32 @@ const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const ee = new EE();
 
+const pool = [];
+
+
+
+ee.on('@dm', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  let message = string.split(' ').slice(1).join(' ').trim();
+
+  pool.forEach( c => {
+    if (c.nickname === nickname) {
+      c.socket.write(`${client.nickname}: ${message}`);
+    }
+  });
+});
+
+ee.on('@all', function(client, string) {
+  pool.forEach(c => {
+      c.socket.write(`${client.nickname}:` + string);
+  });
+});
+
+
+ee.on('default', function(client, string) {
+  client.socket.write('not a command\n');
+});
+
 server.on('connection', function(socket) {
   var client = new Client(socket);
   pool.push(client);
@@ -14,16 +40,17 @@ server.on('connection', function(socket) {
 
   socket.on('data', function(data) {
     const command = data.toString().split(' ').shift().trim();
+    const message = data.toString().split(' ');
 
     if (command.startsWith('@')) {
       ee.emit(command, client, data.toString().split().slice(1).join(' '));
       return;
-    };
+    }
 
     ee.emit('default', client, data.toString());
   });
 });
 
 server.listen(PORT, function() {
-  console.log('server up and running on:', PORT);
+  console.log('Gary chat is up and running on:', PORT);
 });
